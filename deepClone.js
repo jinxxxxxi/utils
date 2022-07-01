63ce9e49d5dd72545af990862cf2213b8bb2d4c3
zhangjing19 <zhangjing19@kuaishou.com>
2022-07-01 16:16:44 +0800

    feat: add deepCLone

---
+++ deepClone.js
@@ -0,0 +1,29 @@
+let a = { a: 'a', b: { c: 'c' }, d: [1, { zj: 'zj' }, 3], date: new Date() }
+function deepClone(obj) {
+  if (!obj) {
+    return
+  }
+  let res = Array.isArray(obj) ? [] : {}
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key]
+    if (value instanceof Date) {
+      res[key] = new Date(value - 0)
+    }
+    if (typeof value === 'object') {
+      if (Array.isArray(value)) {
+        // 数组处理
+        res[key] = deepClone(value)
+      } else {
+        // 递归处理
+        res[key] = deepClone(value)
+      }
+    } else {
+      res[key] = value
+    }
+  })
+
+  return res
+}
+
+let b = deepClone(a)
+console.log(b)
